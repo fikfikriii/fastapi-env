016866d53adbe1428d5d4707c2d9fcbc72b1d6ba
fikfikriii <18220097@std.stei.itb.ac.id>
2022-09-27 20:37:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Items(BaseModel):
+    nama: str
+
+data_mahasiswa = {
+    18220000: {
+        "Nama": "Test"
+    }
+}
 
 app = FastAPI()
 
+@app.get("/mahasiswa/{nim}")
+def get_mahasiswa(nim: int):
+    if nim in data_mahasiswa:
+        return data_mahasiswa[nim]
+    else:
+        return {"Data mahasiswa tidak ditemukan"}
 
-@app.get("/")
-def read_root():
-    return {"NIM": "18220097",
-            "Nama": "Muhamad Fikri Nurohman"
-            }
+@app.post("/mahasiswa")
+def post_mahasiswa(nim: int, item: Items):
+    if nim in data_mahasiswa:
+        return {"Error: Data mahasiswa sudah terdaftar"}
+
+    data_mahasiswa[nim] = {"nama": item.nama}
+    return data_mahasiswa[nim]
